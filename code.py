509706fa69b8a19ed11ff8594ba25621efa7dc86
Jon19777 <mpj18101977@aol.com>
2017-12-29 20:19:21 +0000

    a little concatenation fever

--- code.py
+++ code.py
@@ -1,3 +1,9 @@
 happy_christmas = "Hello World"
 
 print (happy_christmas)
+
+jingle_bells = "my name is Matthew, I hope each one of you that comes past here has a great 2018"
+
+have_a_great_new_year = happy_christmas + jingle_bells
+
+print (have_a_great_new_year)
